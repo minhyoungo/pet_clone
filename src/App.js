4dc2dfba49fbb82313762db0297d32f89d8ebbc0
minhyoungo <59827905+minhyoungo@users.noreply.github.com>
2021-08-30 11:59:12 +0900

    conf

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="WholeWrapper">
+      <div class="header RowWrapper">12345</div>
+
+      <div class="RsWrapper">
+        <div class="Wrapper_1 Wrapper100">
+          <div class="RowWrapper main_1_content">
+            <img
+              class="LGW"
+              src="https://firebasestorage.googleapis.com/v0/b/study-77c83.appspot.com/o/just%2Fpetmart%2F%E1%84%80%E1%85%B3%E1%84%85%E1%85%AE%E1%86%B8%20383.png?alt=media&token=5e1277b0-4ae1-47d9-854b-9fa460a54e7c"
+            />
+            <div class="Wrapper_1_content">
+              <img
+                class="main_1_image_1"
+                src="https://firebasestorage.googleapis.com/v0/b/study-77c83.appspot.com/o/just%2Fpetmart%2Fpetmart_icon.png?alt=media&token=52961d4e-e0c3-44c6-88a1-e0075a53b607"
+                alt=""
+              />
+              <div class="text1 text">반려동물 관련 업종 최초</div>
+              <div class="RowWrapper">
+                <div class="text2 text">한국 프랜차이즈&nbsp;</div>
+                <div class="text3 text">대상&nbsp;</div>
+                <div class="text4 text">수상</div>
+              </div>
+            </div>
+            <img
+              class="RGW"
+              src="https://firebasestorage.googleapis.com/v0/b/study-77c83.appspot.com/o/just%2Fpetmart%2F%E1%84%80%E1%85%B3%E1%84%85%E1%85%AE%E1%86%B8%20384.png?alt=media&token=1e6b8b99-059e-4d35-a034-560a8555f070"
+            />
+          </div>
+          <div className="RowWrapper sub_1_contents">
+            <div className="RowWrapper sub_1_content"></div>
+            <div className="RowWrapper sub_1_content"></div>
+          </div>
+        </div>
+      </div>
+      <div class="footer"></div>
+    </div>
+  );
+};
+
+export default App;
